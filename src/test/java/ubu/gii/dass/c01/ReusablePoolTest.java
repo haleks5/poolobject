45d88d991c0dce969e56e90a85460f8d67e6a822
andrespuentesg <150024530+andrespuentesg@users.noreply.github.com>
2025-02-28 11:18:40 +0100

    Update ReusablePoolTest.java

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -31,47 +31,7 @@ public class ReusablePoolTest {
       		ReusablePool instance2 = ReusablePool.getInstance();
       		assertSame(instance1, instance2);		
 	}
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
-	*/
-		@Test
-    	@DisplayName("testAcquireReusable")
-	public void testAcquireReusable() {
-
-		ReusablePool pool = ReusablePool.getInstance();
-		try {
-
-			Reusable reusable = pool.acquireReusable();
-
-			assertNotNull(reusable,"El objeto reusable adquirido no debería ser nulo");
-
-        	assertTrue(reusable instanceof Reusable, "El objeto adquirido debe ser una instancia de Reusable");
-		} catch (NotFreeInstanceException e) {
-			fail("No se lanzaría una excepción si hemos adquirirido un objeto reusable del pool");
-        }
-		
-	}
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
-		 * @throws NotFreeInstanceException 
-		*/ 
-		 @Test
-		 @DisplayName("testReleaseReusable")
-		 public void testReleaseReusable() throws DuplicatedInstanceException, NotFreeInstanceException {
-	 		ReusablePool pool = ReusablePool.getInstance();
-			Reusable reusable = new Reusable();
-		 try{
-			 pool.releaseReusable(reusable);
-			 Reusable acqReusable = pool.acquireReusable();
-			 
-			 assertEquals(reusable, acqReusable, "El objeto que ha sido adquirido debe ser el mismo que el liberado");
-			 assertNotNull(pool.acquireReusable(), "El pool debería tener al menos un objeto reusable después de liberarlo");
-		 } catch (DuplicatedInstanceException e) {
-			fail("No debería saltar una excepcion al liberar un objeto reusable");
-		 } catch (NotFreeInstanceException e) {
-			fail("No se debe lanzar una excepción al adquirir un objeto reusable");
-		 }
-	 }
+	
 
 	
 	
